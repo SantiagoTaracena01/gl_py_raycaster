2bf911d31ce8e684aa44f0041986b1f8d8296e67
unknown <staracenapuga@gmail.com>
2022-10-25 19:39:45 -0600

    Clase del 25/10 finalizada.

--- src/raycaster.py
+++ src/raycaster.py
@@ -5,18 +5,29 @@ Proyecto 03: Raycaster
 Santiago Taracena Puga (20017)
 """
 
+# Librerías importantes para el desarrollo del raycaster.
 import pygame
-
 from OpenGL.GL import *
+from sympy import evaluate
 
+# Colores necesarios para el raycaster.
+GREEN = (0.0, 1.0, 0.0, 1.0)
+RED = (1.0, 0.0, 0.0, 1.0)
+
+# Inicialización de la librería pygame.
 pygame.init()
 
+# Creación de la pantalla del juego.
 screen = pygame.display.set_mode((800, 600), pygame.OPENGL | pygame.DOUBLEBUF)
 
+# Posición inicial de un pixel.
 x, y = 0, 0
 
+# Estado de ejecución del raycaster y velocidad de los pixeles.
 running = True
+speed = 1
 
+# Función que dibuja un pixel con las tecnologías de OpenGL.
 def pixel(x, y, color):
   glEnable(GL_SCISSOR_TEST)
   glScissor(x, y, 10, 10)
@@ -24,27 +35,27 @@ def pixel(x, y, color):
   glClear(GL_COLOR_BUFFER_BIT)
   glDisable(GL_SCISSOR_TEST)
 
-speed = 1
-
+# Ciclo de "vida" del raycaster.
 while (running):
 
-  # Limpiar
-  glClearColor(0.1, 0.8, 0.2, 1.0)
+  # Limpieza inicial de la pantalla
+  glClearColor(*GREEN)
   glClear(GL_COLOR_BUFFER_BIT)
-  
-  # Pintar
-  pixel(x, 100, (1.0, 0.0, 0.0, 1.0))
-  x += speed
-  
-  if x == 800:
-    speed = -1
-  elif x == 0:
-    speed = 1
-  
-  # Flip
-  pygame.display.flip()
-  
 
+  # Creación de un pixel en la pantalla y movimiento de la misma.
+  pixel(x, 100, RED)
+  x += speed
+
+  # Cambio de la velocidad del raycaster.
+  if (x == 800):
+    speed = -1
+  elif (x == 0):
+    speed = 1
+
+  # Flip del framebuffer de pygame.
+  pygame.display.flip()
+
+  # Posibilidad de cerrar la ventana creada por pygame.
   for event in pygame.event.get():
     if (event.type == pygame.QUIT):
       running = False
